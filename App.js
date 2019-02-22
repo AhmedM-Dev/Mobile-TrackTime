14b2a4a00d7c2a214b6c10d884891b607f2f9101
AsmaBenAhmed <asma.bahmed19@hotmail.com>
2019-02-22 16:24:32 +0100

    Latest changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import Login from './src/components/screens/login';
+import Login from './src/components/screens/Login';
 
 export default class App extends Component {
   render() {
