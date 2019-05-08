074b6ae7a209bc46572aa55e0040ed98844e8388
Ahmed Mahmoudi <ahmed.mahmoudi@proxym-it.com>
2019-05-08 23:47:08 +0100

    Updated the projet to react native 0.59.8

--- android/app/src/main/java/com/tracktimewithoutredux/MainApplication.java
+++ android/app/src/main/java/com/tracktimewithoutredux/MainApplication.java
@@ -1,17 +1,17 @@
-package com.tracktime;
+package com.tracktimewithoutredux;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.bluroverly.SajjadBlurOverlayPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.wheelpicker.WheelPickerPackage;
 import com.zyu.ReactNativeWheelPickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.cardio.RNCardIOPackage;
 import com.horcrux.svg.SvgPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.bluroverly.SajjadBlurOverlayPackage;
+import com.cardio.RNCardIOPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -19,7 +19,6 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
-
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,15 +32,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SajjadBlurOverlayPackage(),
-            new ReanimatedPackage(),
-            new ImagePickerPackage(),
             new WheelPickerPackage(),
             new ReactNativeWheelPickerPackage(),
             new VectorIconsPackage(),
-            new RNCardIOPackage(),
             new SvgPackage(),
-            new RNGestureHandlerPackage()
+            new ReanimatedPackage(),
+            new ImagePickerPackage(),
+            new RNGestureHandlerPackage(),
+            new SajjadBlurOverlayPackage(),
+            new RNCardIOPackage()
       );
     }
 
