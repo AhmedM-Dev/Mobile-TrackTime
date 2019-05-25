916cebb980a42a7504201d9233f1252bffc5cc3f
Ahmed Mahmoudi <ahmed.mahmoudi@proxym-it.com>
2019-05-25 17:04:12 +0100

    Updating new request

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-const address = "10.10.221.145:5000";
+const address = "10.10.221.123:5000";
 
 export const API_URL = `http://${address}/tracktime/api/`;
--- src/components/screens/NewRequest/NewRequest.js
+++ src/components/screens/NewRequest/NewRequest.js
@@ -1,37 +1,31 @@
 
-import React from 'react';
-import { Text, View,Button } from 'react-native';
 import { createMaterialTopTabNavigator } from 'react-navigation';
 import LeaveRequest from './LeaveRequest'
 import AttendanceCorrection from './Attendance correction'
 
-
- export default createMaterialTopTabNavigator({
-    'Attendance correction Request': { 
-    screen: AttendanceCorrection
-   },
-   'Leave request': { 
+export default createMaterialTopTabNavigator({
+  'New Request': {
     screen: LeaveRequest
   },
-},
-{
-  tabBarOptions: {
-    activeTintColor : 'black',
-    inactiveTintColor:'black',
-    pressColor :'#2CA96E',
-    tabStyle :{
-
-    },
-    indicatorStyle :{
-      backgroundColor:'#2CA96E'
-    },
-  
-    style: {
-      backgroundColor:'white'
-    }
+  'Attendance correction Request': {
+    screen: AttendanceCorrection
   },
-});
+},
+  {
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'black',
+      pressColor: '#2CA96E',
+      tabStyle: {
 
+      },
 
+      indicatorStyle: {
+        backgroundColor: '#2CA96E'
+      },
 
-
+      style: {
+        backgroundColor: 'white'
+      }
+    },
+  });
